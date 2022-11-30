65e23c13bd4dc37d633ab3aecbb6c041410199c6
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-11-30 11:58:14 +0100

    add camera and ground

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use bevy::{
     diagnostic::{EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin},
     prelude::*,
 };
-
+use bevy_inspector_egui::WorldInspectorPlugin;
 use kingdoms_arena::AppPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(FrameTimeDiagnosticsPlugin)
         .add_plugin(EntityCountDiagnosticsPlugin)
         .add_plugin(AppPlugin)
