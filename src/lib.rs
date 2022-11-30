cb9f56351f6e8ef2aad38cae18e36c3fcc1b8ee2
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-11-30 11:58:14 +0100

    add camera and ground

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-use bevy::prelude::*;
+mod palette;
+
+use bevy::{
+    core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode,
+    sprite::Anchor,
+};
+
+const WORLD_HEIGHT: f32 = 12.0;
+const GROUND_HEIGHT: f32 = 6.0;
+const CAMERA_HEIGHT: f32 = (WORLD_HEIGHT - GROUND_HEIGHT) / 2.0;
+const CAMERA_SIZE: f32 = WORLD_HEIGHT + GROUND_HEIGHT;
 
 pub struct AppPlugin;
 
@@ -13,4 +23,28 @@ enum AppState {
     Game,
 }
 
-fn setup() {}
+fn setup(mut commands: Commands) {
+    // camera
+    commands.spawn(Camera2dBundle {
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::Custom(palette::LIGHT_CYAN),
+        },
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical(CAMERA_SIZE),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, CAMERA_HEIGHT, 0.0),
+        ..default()
+    });
+    // ground
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: palette::DARK_GREEN,
+            custom_size: Some(Vec2::new(100.0, GROUND_HEIGHT)),
+            anchor: Anchor::TopCenter,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
+}
