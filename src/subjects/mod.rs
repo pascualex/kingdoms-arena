e7453d050751f2a9c7f64a5f7dd0310013e63cba
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-02 19:03:41 +0100

    move weapons to plugin

--- src/subjects/mod.rs
+++ src/subjects/mod.rs
@@ -1,24 +1,23 @@
 pub mod states;
+pub mod weapons;
 
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-use crate::{
-    collisions::{intersections_with, ColliderBundle},
-    palette, Kingdom,
-};
+use crate::{collisions::intersections_with, Kingdom};
 
-use self::states::{MovingState, ShootingState, SubjectStatesPlugin};
+use self::{
+    states::{MovingState, ShootingState, SubjectStatesPlugin},
+    weapons::WeaponsPlugin,
+};
 
 pub struct SubjectsPlugin;
 
 impl Plugin for SubjectsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(SubjectStatesPlugin)
+            .add_plugin(WeaponsPlugin)
             .init_resource::<Frontlines>()
-            .add_system(recharge_bows)
-            .add_system(shoot_subject_bows)
-            .add_system(move_arrows)
             .add_system(move_subjects)
             .add_system(update_frontlines.after(move_subjects))
             .add_system(perform_subject_attacks)
@@ -86,60 +85,6 @@ pub struct Speed {
 impl Speed {
     pub fn new(value: f32) -> Self {
         Self { value }
-    }
-}
-
-#[derive(Component)]
-pub struct Bow {
-    timer: Timer,
-}
-
-impl Bow {
-    pub fn new(cooldown_seconds: f32) -> Self {
-        Self {
-            timer: Timer::from_seconds(cooldown_seconds, TimerMode::Once),
-        }
-    }
-}
-
-#[derive(Component)]
-pub struct Arrow;
-
-fn recharge_bows(mut query: Query<&mut Bow>, time: Res<Time>) {
-    for mut bow in &mut query {
-        bow.timer.tick(time.delta());
-    }
-}
-
-fn shoot_subject_bows(
-    mut query: Query<(&Transform, &mut Bow), (With<Subject>, With<ShootingState>)>,
-    mut commands: Commands,
-) {
-    for (transform, mut bow) in &mut query {
-        if !bow.timer.finished() {
-            continue;
-        }
-        bow.timer.reset();
-        commands.spawn((
-            Name::new("Arrow"),
-            SpriteBundle {
-                sprite: Sprite {
-                    color: palette::DARK_YELLOW,
-                    custom_size: Some(Vec2::new(0.1, 0.1)),
-                    ..default()
-                },
-                transform: Transform::from_translation(transform.translation),
-                ..default()
-            },
-            ColliderBundle::kinematic(Collider::cuboid(0.05, 0.05)),
-            Arrow,
-        ));
-    }
-}
-
-fn move_arrows(mut query: Query<&mut Transform, With<Arrow>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        transform.translation.x += time.delta_seconds() * 5.0;
     }
 }
 
