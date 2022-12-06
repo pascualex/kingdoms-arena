5ebca12c1e9ee5a6720ff91cfbb27e5e884bbb73
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-06 12:11:14 +0100

    add bow sound with bevy_kira

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use bevy::{
     prelude::*,
 };
 use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_kira_audio::prelude::*;
 use bevy_rapier2d::prelude::*;
 use kingdoms_arena::AppPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(AudioPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin {
             mode: DebugRenderMode::COLLIDER_SHAPES,
