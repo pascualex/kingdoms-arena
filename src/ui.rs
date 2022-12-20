fcee08639bf4104d09d23d41f8642090e9a3ef9a
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-20 12:15:55 +0100

    add 3 spawn buttons

--- src/ui.rs
+++ src/ui.rs
@@ -14,20 +14,40 @@ fn setup(mut commands: Commands) {
     let root = NodeBundle {
         style: Style {
             margin: UiRect::new(Val::Auto, Val::Auto, Val::Auto, Val::Px(40.0)),
+            // TODO: add gap when bevy upgrades to taffy v0.2
             ..default()
         },
         ..default()
     };
-    let button = ButtonBundle {
+    let button_1 = ButtonBundle {
         style: Style {
-            size: Size::new(Val::Px(200.0), Val::Px(80.0)),
+            size: Size::new(Val::Px(80.0), Val::Px(80.0)),
             ..default()
         },
-        background_color: palette::DARK_RED.into(),
+        background_color: palette::DARK_BLUE.into(),
+        ..default()
+    };
+    let button_2 = ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(80.0), Val::Px(80.0)),
+            margin: UiRect::new(Val::Px(20.0), Val::Px(20.0), Val::Undefined, Val::Undefined),
+            ..default()
+        },
+        background_color: palette::DARK_YELLOW.into(),
+        ..default()
+    };
+    let button_3 = ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(80.0), Val::Px(80.0)),
+            ..default()
+        },
+        background_color: palette::DARK_ORANGE.into(),
         ..default()
     };
     commands.spawn(root).with_children(|builder| {
-        builder.spawn(button);
+        builder.spawn(button_1);
+        builder.spawn(button_2);
+        builder.spawn(button_3);
     });
 }
 
