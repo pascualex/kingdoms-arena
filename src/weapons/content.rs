ed5d00406f1cb51c87c8d027c819597b77acbaac
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-21 14:38:58 +0100

    add damage to swords

--- src/weapons/content.rs
+++ src/weapons/content.rs
@@ -1,36 +1,51 @@
-pub enum WeaponsBlueprint {
+pub struct WeaponBlueprint {
+    pub damage: u32,
+    pub kind: WeaponKind,
+}
+
+pub enum WeaponKind {
     Sword,
-    Bow(&'static BowBlueprint),
+    Bow(BowBlueprint),
 }
 
 pub struct BowBlueprint {
-    pub damage: u32,
     pub range: f32,
     pub speed: f32,
     pub spread: f32,
     pub recharge_seconds: f32,
 }
 
-pub const ELVEN_BOW: BowBlueprint = BowBlueprint {
+pub const ELVEN_BOW: WeaponBlueprint = WeaponBlueprint {
     damage: 1,
-    range: 15.0,
-    speed: 15.0,
-    spread: 1.5,
-    recharge_seconds: 2.3,
+    kind: WeaponKind::Bow(BowBlueprint {
+        range: 15.0,
+        speed: 15.0,
+        spread: 1.5,
+        recharge_seconds: 2.3,
+    }),
 };
 
-pub const ELVEN_FAST_BOW: BowBlueprint = BowBlueprint {
+pub const ELVEN_FAST_BOW: WeaponBlueprint = WeaponBlueprint {
     damage: 1,
-    range: 10.0,
-    speed: 25.0,
-    spread: 2.0,
-    recharge_seconds: 0.3,
+    kind: WeaponKind::Bow(BowBlueprint {
+        range: 10.0,
+        speed: 25.0,
+        spread: 2.0,
+        recharge_seconds: 0.3,
+    }),
 };
 
-pub const ELVEN_SNIPER_BOW: BowBlueprint = BowBlueprint {
+pub const ELVEN_SNIPER_BOW: WeaponBlueprint = WeaponBlueprint {
     damage: 2,
-    range: 100.0,
-    speed: 15.0,
-    spread: 0.0,
-    recharge_seconds: 0.0,
+    kind: WeaponKind::Bow(BowBlueprint {
+        range: 100.0,
+        speed: 15.0,
+        spread: 0.0,
+        recharge_seconds: 0.0,
+    }),
+};
+
+pub const GOBLIN_SWORD: WeaponBlueprint = WeaponBlueprint {
+    damage: 1,
+    kind: WeaponKind::Sword,
 };
