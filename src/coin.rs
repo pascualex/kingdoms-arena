948b5c03dfadfe4597d28b2fc93204dffa18aeb6
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-23 10:00:21 +0100

    reset coins when game finishes

--- src/coin.rs
+++ src/coin.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
 
+use crate::AppState;
+
 pub struct CoinPlugin;
 
 impl Plugin for CoinPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Coins>().add_system(generate_coins);
+        app.init_resource::<Coins>()
+            .add_system_set(SystemSet::on_exit(AppState::Game).with_system(reset_coins))
+            .add_system_set(SystemSet::on_update(AppState::Game).with_system(generate_coins));
     }
 }
 
@@ -21,4 +25,8 @@ impl Coins {
 
 fn generate_coins(mut coins: ResMut<Coins>, time: Res<Time>) {
     coins.elven += time.delta_seconds();
+}
+
+fn reset_coins(mut coins: ResMut<Coins>) {
+    coins.elven = 0.0;
 }
